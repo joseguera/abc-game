63417b44c8de2207514cf454965965ee7d300742
Joseguera <joseguera@uclaextension.edu>
2022-12-23 10:10:31 -0800

    added an audio hook to create an audio object that took an audio file from the animalFacts array based on the factCounter hook. This new audio hook allowed me to obtain the duration property for each audio object. The duration property will allow me to disable the FactButton for the precise duration of the fact playing.

--- src/components/FactButton/FactButton.js
+++ src/components/FactButton/FactButton.js
@@ -1,22 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Icon, IconDisabled } from "./FactButton.styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
 
 const FactButton = (props) => {
   const [factCounter, setFactCounter] = useState(0);
+  const [audio, setAudio] = useState(typeof Audio !== "undefined" && new Audio(props.animalFacts[0]));
+  // const [isPlaying, setIsPlaying] = useState(false);
 
   const adjustCounter = () => {
     setFactCounter((currentCount) => {
       let factNumber = currentCount >= 2 ? 0 : (currentCount += 1);
+      setAudioTrack(factNumber)
       return factNumber;
     });
   };
 
-  const playAudio = (count) => {
-    console.log({ count });
-    return new Audio(props.animalFacts[count]).play();
+  const setAudioTrack = (factNumber) => {
+    setAudio((currentElement) => {
+      currentElement = new Audio(props.animalFacts[factNumber]);
+      return currentElement;
+    });
   };
+
+  const playAudio = () => {
+    console.log(audio.src, audio.duration, factCounter);
+    return audio.play();
+  };
+
+  // useEffect(() => {
+  //   isPlaying ? audio.play() : audio.pause();
+  // }, [isPlaying]);
 
   return (
     <>
@@ -25,14 +39,12 @@ const FactButton = (props) => {
           icon={faLightbulb}
           onClick={() => {
             adjustCounter();
-            playAudio(factCounter);
+            playAudio();
           }}
         />
       </Icon>
       <IconDisabled>
-        <FontAwesomeIcon
-          icon={faLightbulb}
-        />
+        <FontAwesomeIcon icon={faLightbulb} />
       </IconDisabled>
     </>
   );
