52362e27e448b023967a68b0af1c1fd1d5a30f84
Joseguera <joseguera@uclaextension.edu>
2022-12-26 10:53:58 -0800

    added persistStore to redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit'
 import playingReducer from '../features/playing/playingSlice'
+import likeReducer from '../features/like/likeSlice'
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
 
-export default configureStore({
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  like: likeReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
   reducer: {
-    playing: playingReducer
-  }
-})
+    persistedReducer,
+    playing: playingReducer,
+  },
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
