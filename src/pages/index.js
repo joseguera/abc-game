71c2af077e5dde8c4944326618345d9f30b781dc
Joseguera <joseguera@uclaextension.edu>
2023-01-21 10:10:05 -0800

    connected STEAM and Home pages to their respective routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
-export { default as Home } from "./Home";
-export { default as Science } from "./Science";
-export { default as Technology } from "./Technology";
-export { default as Engineering } from "./Engineering";
-export { default as Arts } from "./Arts";
-export { default as Math } from "./Math";
+export { default as Home } from './Home'
+export { default as Science } from './Science'
+export { default as Technology } from './Technology'
+export { default as Engineering } from './Engineering'
+export { default as Arts } from './Arts'
+export { default as Math } from './Math'
 // export { default as MobileSearch } from "./MobileSearch";
-
