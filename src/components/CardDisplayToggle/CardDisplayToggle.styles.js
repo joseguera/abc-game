c3f804edefd12fa04a663fdfcc579222bb9321ac
Joseguera <joseguera@uclaextension.edu>
2023-05-19 11:27:50 -0700

    styled the CardDisplayToggle component. Aside from making the intended functionality work, I'd also like to make the sliding transition smoother

--- src/components/CardDisplayToggle/CardDisplayToggle.styles.js
+++ src/components/CardDisplayToggle/CardDisplayToggle.styles.js
@@ -6,16 +6,41 @@ export const Toggler = styled.div`
   align-self: center;
   align-items: center;
   justify-content: space-around;
-  border: 1px solid black;
-  border-radius: 10px;
-  gap: 10px;
+  border: 1px solid #3d674c;
+  border-radius: 6px;
   height: 32px;
-  width: 75px;
+  width: 84px;
+  transition: all 5s;
+
+`;
+
+export const SelectedIcon = styled.p`
+  font-size: 22px;
+  width: 40px;
+  height: 30px;
+  text-align: center;
+  color: #fde7b4;
+  background-color: #3d674c;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:hover,
+  &:focus {
+    cursor: pointer;
+  }
 `;
 
 export const Icon = styled.p`
   font-size: 22px;
-  color: #3d674c /*Spring Leaves*/;
+  width: 40px;
+  height: 30px;
+  text-align: center;
+  color: #3d674c;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   &:hover,
   &:focus {
     cursor: pointer;
