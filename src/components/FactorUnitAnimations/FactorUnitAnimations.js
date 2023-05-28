bcc8228cf5c1dc52e89547c5ccc176f70971aec6
Joseguera <joseguera@uclaextension.edu>
2023-05-28 16:03:54 -0700

    Finally got mu dotOrganizer method to work as intended. Still need to figure out a way of displaying the images vertically (currently displaying horizontally), possibly rotating the container div. I also eed to find a way of displaying the images as 1 group of the number by default as well as find a way of displaying the number in groups of 1.

--- src/components/FactorUnitAnimations/FactorUnitAnimations.js
+++ src/components/FactorUnitAnimations/FactorUnitAnimations.js
@@ -1,7 +1,7 @@
 import React from "react";
 import apple from '../../images/numbers/apple.png';
 import { dotOrganizer } from "../../utils/utils.js"
-import { DotPicture, UnitImage, DotHolder, Dot } from "./FactorUnitAnimations.styles";
+import { DotPicture, UnitImage, UnitHolder, Dot } from "./FactorUnitAnimations.styles";
 
 export default function FactorUnitAnimations({ unitNumber, button, value }) {
   const cups = Array.apply(null, Array(unitNumber / value)).map(function () {});
@@ -12,19 +12,43 @@ export default function FactorUnitAnimations({ unitNumber, button, value }) {
 
   const dotArray = dotOrganizer(value);
 
+  console.log(dotArray);
+
+  const dots = dotArray.map(dot => {
+    return (
+      Array.apply(null, Array(dot)).map(function () {})
+    )
+  })
+
+  console.log(dots);
 
   return (
     <DotPicture>
-      {cups.map((el, index) => {
-        return (
-          <div className={value > 5 ? "smallNumber" : "bigNumber"} key={index}>
-            {beads.map((el2, index2) => {
-              // return <Dot key={index2}></Dot>;
-              return <UnitImage key={index2} src={apple} />
-            })}
-          </div>
-        );
-      })}
+    {cups.map((el, index) => {
+      return (
+        dots.map((dot, index) => {
+          return (
+            <UnitHolder>
+              {dot.map(d => {
+                return <UnitImage src={apple}/>
+              })}
+            </UnitHolder>
+          )
+        })
+      )
+    })}
     </DotPicture>
+    // <DotPicture>
+    //   {cups.map((el, index) => {
+    //     return (
+    //       <div className={value > 5 ? "smallNumber" : "bigNumber"} key={index}>
+    //         {beads.map((el2, index2) => {
+    //           // return <Dot key={index2}></Dot>;
+    //           return <UnitImage key={index2} src={apple} />
+    //         })}
+    //       </div>
+    //     );
+    //   })}
+    // </DotPicture>
   );
 }
