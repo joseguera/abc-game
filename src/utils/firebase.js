d5b4bf8735110098db5fd473f95594b60181c969
Joseguera <joseguera@uclaextension.edu>
2023-06-10 17:47:49 -0700

    added dotenv variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,13 +9,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCQok51QbBhsDTtBSAbyvJn48HlmQiOFVg",
-  authDomain: "aby-zebra.firebaseapp.com",
-  projectId: "aby-zebra",
-  storageBucket: "aby-zebra.appspot.com",
-  messagingSenderId: "426018360983",
-  appId: "1:426018360983:web:7f1196b3f0c72552d823d2",
-  measurementId: "G-2JNHZRCX7Z"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
