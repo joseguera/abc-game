4edc370cd8018fe20aaa597ae6ecc784dc962e04
Joseguera <joseguera@uclaextension.edu>
2023-07-15 20:50:53 -0700

    Updated Menu modal component to a native HTML dialog element. Need to style it further

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,30 +1,29 @@
 import React from "react";
 import { ModalBackground, MenuHolder, MenuItem, StyledLink } from "./Menu.styles";
 
-export default function Menu({ openMenu }) {
+export default function Menu({ menuModal }) {
 
   return (
-    <>
-      <ModalBackground onClick={() => openMenu()}/>
+    <dialog ref={menuModal} autoFocus method="dialog">
       <MenuHolder>
-        <div className="show">
-          <StyledLink to="/" onClick={() => openMenu()}>
+        <div>
+          <StyledLink to="/" onClick={() => menuModal.current.close()}>
             <MenuItem >Home</MenuItem>
           </StyledLink>
-          <StyledLink to="/about" onClick={() => openMenu()}>
+          <StyledLink to="/about" onClick={() => menuModal.current.close()}>
             <MenuItem>About</MenuItem>
           </StyledLink>
-          <StyledLink to="/aby-store" onClick={() => openMenu()}>
+          <StyledLink to="/aby-store" onClick={() => menuModal.current.close()}>
             <MenuItem>Aby's Store</MenuItem>
           </StyledLink>
-          <StyledLink to="/my-favorites" onClick={() => openMenu()}>
+          <StyledLink to="/my-favorites" onClick={() => menuModal.current.close()}>
             <MenuItem>My Favorites</MenuItem>
           </StyledLink>
-          <StyledLink to="/contact" onClick={() => openMenu()}>
+          <StyledLink to="/contact" onClick={() => menuModal.current.close()}>
             <MenuItem>Contact</MenuItem>
           </StyledLink>
         </div>
       </MenuHolder>
-    </>
+    </dialog>
   );
 }
