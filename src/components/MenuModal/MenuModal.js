8e337879bee756b47594a7b0342c61f5a0e92bef
Joseguera <joseguera@uclaextension.edu>
2023-09-14 22:15:57 -0700

    finished MenuModal tailwind migration

--- src/components/MenuModal/MenuModal.js
+++ src/components/MenuModal/MenuModal.js
@@ -1,33 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { MenuItem } from "./MenuModal.styles";
 
 export default function MenuModal({ menuModal }) {
   return (
     <dialog ref={menuModal} autoFocus method="dialog">
       <div>
-        <Link className="no-underline text-inherit hover:text-[#ff6347]" to="/" onClick={() => menuModal.current.close()}>
-          <MenuItem>Home</MenuItem>
+        <Link
+          className="no-underline text-inherit hover:text-[#ff6347]"
+          to="/"
+          onClick={() => menuModal.current.close()}
+        >
+          <div className="flex leading-10 text-3xl cursor-pointer justify-center">
+            Home
+          </div>
         </Link>
-        <Link className="no-underline text-inherit hover:text-[#ff6347]" to="/about" onClick={() => menuModal.current.close()}>
-          <MenuItem>About</MenuItem>
+        <Link
+          className="no-underline text-inherit hover:text-[#ff6347]"
+          to="/about"
+          onClick={() => menuModal.current.close()}
+        >
+          <div className="flex leading-10 text-3xl cursor-pointer justify-center">
+            About
+          </div>
         </Link>
-        <Link className="no-underline text-inherit hover:text-[#ff6347]" to="/aby-store" onClick={() => menuModal.current.close()}>
-          <MenuItem>Aby's Store</MenuItem>
+        <Link
+          className="no-underline text-inherit hover:text-[#ff6347]"
+          to="/aby-store"
+          onClick={() => menuModal.current.close()}
+        >
+          <div className="flex leading-10 text-3xl cursor-pointer justify-center">
+            Aby's Store
+          </div>
         </Link>
         <Link
           className="no-underline text-inherit hover:text-[#ff6347]"
           to="/my-favorites"
           onClick={() => menuModal.current.close()}
         >
-          <MenuItem>My Favorites</MenuItem>
+          <div className="flex leading-10 text-3xl cursor-pointer justify-center">
+            My Favorites
+          </div>
         </Link>
-        <Link className="no-underline text-inherit hover:text-[#ff6347]" to="/contact" onClick={() => menuModal.current.close()}>
-          <MenuItem>Contact</MenuItem>
+        <Link
+          className="no-underline text-inherit hover:text-[#ff6347]"
+          to="/contact"
+          onClick={() => menuModal.current.close()}
+        >
+          <div className="flex leading-10 text-3xl cursor-pointer justify-center">
+            Contact
+          </div>
         </Link>
-        <MenuItem>
-          <span className="close-modal" onClick={() => menuModal.current.close()}>&#x2715;</span>
-        </MenuItem>
+        <div className="flex leading-10 text-3xl cursor-pointer justify-center">
+          <span
+            className="close-modal"
+            onClick={() => menuModal.current.close()}
+          >
+            &#x2715;
+          </span>
+        </div>
       </div>
     </dialog>
   );
