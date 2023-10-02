d4c9c5706e6d0c333e543e244e4304c4ecc9320c
Joseguera <joseguera@uclaextension.edu>
2023-10-02 12:35:33 -0700

    fixed like button

--- src/components/LikeButton/LikeButton.js
+++ src/components/LikeButton/LikeButton.js
@@ -1,32 +1,32 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useOutletContext } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
+
 export default function LikeButton({
-  handleLike,
   isLiked,
-  id,
-  category,
-  ...props
+  item
 }) {
+  const { handleLike } = useOutletContext();
   const playing = useSelector((state) => state.playing.value);
-  const handleLikeHeart = (id, category, list) => {
-    handleLike(id, category, list);
+  const handleLikeHeart = (id, category, array) => {
+    handleLike(id, category, array);
   };
 
   return playing ? (
     isLiked ? (
       <div
         className="text-lg cursor-pointer text-[#ff6347] hover:text-[#ff9380]" 
-        onClick={() => handleLikeHeart(id, category, props.list)}
+        onClick={() => handleLikeHeart(item.id, item.category, item)}
       >
         <FontAwesomeIcon icon={faHeart} />
       </div>
     ) : (
       <div
         className="text-lg cursor-pointer text-[#2f4f4f] hover:text-[#4d8080]"
-        onClick={() => handleLikeHeart(id, category, props.list)}
+        onClick={() => handleLikeHeart(item.id, item.category, item)}
       >
         <FontAwesomeIcon icon={faHeart} />
       </div>
