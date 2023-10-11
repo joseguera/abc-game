b7c85cfd54ba9663c4f0ecaed2748b740ae780b0
Joseguera <joseguera@uclaextension.edu>
2023-10-10 20:22:01 -0700

    Updated LetterTile and SyllableTile buttons

--- src/components/LetterTile/LetterTile.js
+++ src/components/LetterTile/LetterTile.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Tile, Dash } from "./LetterTile.styles";
 
 export default function LetterTile({ letter, letterSound }) {
   const letters = letter.toUpperCase();
@@ -7,8 +6,10 @@ export default function LetterTile({ letter, letterSound }) {
     new Audio(letterSound[letters]).play();
   };
   return letter !== "-" ? (
-    <Tile onClick={playAudio}>{letter}</Tile>
+    <div onClick={playAudio} className="w-11 h-11 drop-shadow-md bg-[#e3dac9] leading-10 text-4xl text-center border border-[#3d674c] rounded-lg cursor-pointer hover:drop-shadow-lg hover:text-4xl hover:text-[#558f69]">
+      {letter}
+    </div>
   ) : (
-    <Dash>-</Dash>
+    <div className="w-2.5 text-4xl h-10 leading-10 cursor-auto border-none">-</div>
   );
 }
