7e6549227706746c4701bc4bfb465f5815456d89
Joseguera <joseguera@uclaextension.edu>
2024-01-13 18:29:37 -0800

    Fixed LanguageList hovered state

--- src/components/LanguageList/LanguageList.tsx
+++ src/components/LanguageList/LanguageList.tsx
@@ -6,23 +6,18 @@ interface LangList {
     lang: string;
     text: string;
     selected: boolean;
+    hovered: boolean;
 }
 
 interface LanguageListProps {
   getLangPopup: (option: boolean) => void;
   getSelected: (lang: string) => void;
+  getHovered: (lang: string) => void;
   langList: LangList[];
 }
 
-const LanguageList: React.FC<LanguageListProps> = ({ getLangPopup, getSelected, langList }) => {
+const LanguageList: React.FC<LanguageListProps> = ({ getLangPopup, getSelected, getHovered, langList }) => {
   const dispatch = useDispatch();
-  const [langHover, setLangHover] = useState("w-5 h-5");
-
-function getHovered() {
-    langList.map(lang => {
-        return lang.selected === true ? setLangHover("w-3 h-3") : setLangHover("w-5 h-5"); 
-    })
-} 
 
   return (
     <div className="absolute top-12 bg-[#fcd783] shadow-xl rounded-lg w-fit h-fit z-10 shadow-xl p-4">
@@ -36,12 +31,13 @@ function getHovered() {
                 dispatch(change(lang.lang));
                 getLangPopup(false);
             }}
-            onMouseOver={() => getHovered()}
+            onMouseOver={() => getHovered(lang.lang)}
+            onMouseOut={() => getHovered(lang.lang)}
           >
             <div className="w-5 h-5 rounded-full border-2 border-[#ff6347] flex justify-center items-center">
-              <div
-                className={`${langHover} rounded-full bg-[${lang.selected ? "#ff6347" : "none"}]`}
-              ></div>
+              <div className={`w-5 h-5 rounded-full bg-[${lang.selected ? "#ff6347" : "none"}] flex items-center justify-center`}>
+                <div className={`${lang.hovered ? "w-3 h-3" : "w-0 h-0" } rounded-full bg-[#ff6347]`}></div>
+              </div>
             </div>
             <div className="text-[24px] hover:text-[#ff6347]">{lang.text}</div>
           </div>
