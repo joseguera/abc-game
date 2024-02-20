502a25763675d580ab42acf241d597e896b982a0
Jose Oseguera <joseoseguera@Joses-MBP.lan>
2024-02-19 20:55:45 -0800

    message

---
+++ src/components/FactorButtons/FactorButtons.js
@@ -0,0 +1,47 @@
+import React from "react";
+import _ from "lodash";
+import {
+  factorFinder,
+  primeFactorization,
+  removeDuplicates,
+} from "../../utils/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRectangleXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  ButtonContainer,
+  FactorHolder,
+  NumberButton,
+  CloseIcon,
+} from "./FactorButtons.styles";
+
+export default function FactorButtons({ item, xSectionCloser, ...props }) {
+  const factors = factorFinder(primeFactorization(item.id));
+  const factorButtons = removeDuplicates(factors);
+  const factorValues = [...factorButtons].reverse();
+
+  const factorSplit = (button, value) => {
+    props.factorSplit(button, value);
+  };
+
+  return (
+    <ButtonContainer>
+      <FactorHolder>
+        {_.zipWith(factorButtons, factorValues, function (button, value) {
+          return (
+            <NumberButton
+              key={button}
+              onClick={() => {
+                factorSplit(button, value);
+              }}
+            >
+              {button}
+            </NumberButton>
+          );
+        })}
+      </FactorHolder>
+      <CloseIcon onClick={() => xSectionCloser()}>
+        <FontAwesomeIcon icon={faRectangleXmark} />
+      </CloseIcon>
+    </ButtonContainer>
+  );
+}
